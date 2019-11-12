f5a1e73af9b807c64922b3d886daa3d41a19819e
Jeremy Castagno <jeremybyu@gmail.com>
2019-11-12 13:01:04 -0500

    Update domains

--- src/rs-motion.cpp
+++ src/rs-motion.cpp
@@ -15,245 +15,262 @@
 
 struct short3
 {
-    uint16_t x, y, z;
+	uint16_t x, y, z;
 };
 class rotation_estimator
 {
-    // theta is the angle of camera rotation in x, y and z components
-    float3 theta;
-    std::mutex theta_mtx;
-    /* alpha indicates the part that gyro and accelerometer take in computation of theta; higher alpha gives more weight to gyro, but too high
-    values cause drift; lower alpha gives more weight to accelerometer, which is more sensitive to disturbances */
-    float alpha = 0.98;
-    bool first = true;
-    // Keeps the arrival time of previous gyro frame
-    double last_ts_gyro = 0;
+	// theta is the angle of camera rotation in x, y and z components
+	float3 theta;
+	std::mutex theta_mtx;
+	/* alpha indicates the part that gyro and accelerometer take in computation of theta; higher alpha gives more weight to gyro, but too high
+	values cause drift; lower alpha gives more weight to accelerometer, which is more sensitive to disturbances */
+	float alpha = 0.98;
+	bool first = true;
+	// Keeps the arrival time of previous gyro frame
+	double last_ts_gyro = 0;
 
 public:
-    // Function to calculate the change in angle of motion based on data from gyro
-    void process_gyro(rs2_vector gyro_data, double ts)
-    {
-        if (first) // On the first iteration, use only data from accelerometer to set the camera's initial position
-        {
-            last_ts_gyro = ts;
-            return;
-        }
-        // Holds the change in angle, as calculated from gyro
-        float3 gyro_angle;
-        // Initialize gyro_angle with data from gyro
-        gyro_angle.x = gyro_data.x; // Pitch
-        gyro_angle.y = gyro_data.y; // Yaw
-        gyro_angle.z = gyro_data.z; // Roll
-        // Compute the difference between arrival times of previous and current gyro frames
-        double dt_gyro = (ts - last_ts_gyro) / 1000.0;
-        last_ts_gyro = ts;
-        // Change in angle equals gyro measures * time passed since last measurement
-        gyro_angle = gyro_angle * dt_gyro;
-        // Apply the calculated change of angle to the current angle (theta)
-        std::lock_guard<std::mutex> lock(theta_mtx);
-        theta.add(-gyro_angle.z, -gyro_angle.y, gyro_angle.x);
-    }
+	// Function to calculate the change in angle of motion based on data from gyro
+	void process_gyro(rs2_vector gyro_data, double ts)
+	{
+		if (first) // On the first iteration, use only data from accelerometer to set the camera's initial position
+		{
+			last_ts_gyro = ts;
+			return;
+		}
+		// Holds the change in angle, as calculated from gyro
+		float3 gyro_angle;
+		// Initialize gyro_angle with data from gyro
+		gyro_angle.x = gyro_data.x; // Pitch
+		gyro_angle.y = gyro_data.y; // Yaw
+		gyro_angle.z = gyro_data.z; // Roll
+		// Compute the difference between arrival times of previous and current gyro frames
+		double dt_gyro = (ts - last_ts_gyro) / 1000.0;
+		last_ts_gyro = ts;
+		// Change in angle equals gyro measures * time passed since last measurement
+		gyro_angle = gyro_angle * dt_gyro;
+		// Apply the calculated change of angle to the current angle (theta)
+		std::lock_guard<std::mutex> lock(theta_mtx);
+		theta.add(-gyro_angle.z, -gyro_angle.y, gyro_angle.x);
+	}
 
-    void process_accel(rs2_vector accel_data)
-    {
-        // Holds the angle as calculated from accelerometer data
-        float3 accel_angle;
-        // Calculate rotation angle from accelerometer data
-        accel_angle.z = atan2(accel_data.y, accel_data.z);
-        accel_angle.x = atan2(accel_data.x, sqrt(accel_data.y * accel_data.y + accel_data.z * accel_data.z));
-        // If it is the first iteration, set initial pose of camera according to accelerometer data (note the different handling for Y axis)
-        std::lock_guard<std::mutex> lock(theta_mtx);
-        if (first)
-        {
-            first = false;
-            theta = accel_angle;
-            // Since we can't infer the angle around Y axis using accelerometer data, we'll use PI as a convetion for the initial pose
-            theta.y = PI;
-        }
-        else
-        {
-            /* 
+	void process_accel(rs2_vector accel_data)
+	{
+		// Holds the angle as calculated from accelerometer data
+		float3 accel_angle;
+		// Calculate rotation angle from accelerometer data
+		accel_angle.z = atan2(accel_data.y, accel_data.z);
+		accel_angle.x = atan2(accel_data.x, sqrt(accel_data.y * accel_data.y + accel_data.z * accel_data.z));
+		// If it is the first iteration, set initial pose of camera according to accelerometer data (note the different handling for Y axis)
+		std::lock_guard<std::mutex> lock(theta_mtx);
+		if (first)
+		{
+			first = false;
+			theta = accel_angle;
+			// Since we can't infer the angle around Y axis using accelerometer data, we'll use PI as a convetion for the initial pose
+			theta.y = PI;
+		}
+		else
+		{
+			/*
 
-            Apply Complementary Filter:
+			Apply Complementary Filter:
 
-                - high-pass filter = theta * alpha:  allows short-duration signals to pass through while filtering out signals
+				- high-pass filter = theta * alpha:  allows short-duration signals to pass through while filtering out signals
 
-                  that are steady over time, is used to cancel out drift.
+				  that are steady over time, is used to cancel out drift.
 
-                - low-pass filter = accel * (1- alpha): lets through long term changes, filtering out short term fluctuations 
+				- low-pass filter = accel * (1- alpha): lets through long term changes, filtering out short term fluctuations
 
-            */
-            theta.x = theta.x * alpha + accel_angle.x * (1 - alpha);
-            theta.z = theta.z * alpha + accel_angle.z * (1 - alpha);
-        }
-    }
+			*/
+			theta.x = theta.x * alpha + accel_angle.x * (1 - alpha);
+			theta.z = theta.z * alpha + accel_angle.z * (1 - alpha);
+		}
+	}
 
-    // Returns the current rotation angle
+	// Returns the current rotation angle
 
-    float3 get_theta()
-    {
-        std::lock_guard<std::mutex> lock(theta_mtx);
-        return theta;
-    }
+	float3 get_theta()
+	{
+		std::lock_guard<std::mutex> lock(theta_mtx);
+		return theta;
+	}
 };
 
 bool check_imu_is_supported()
 
 {
 
-    bool found_gyro = false;
-    bool found_accel = false;
-    rs2::context ctx;
-    for (auto dev : ctx.query_devices())
-    {
-        // The same device should support gyro and accel
-        found_gyro = false;
-        found_accel = false;
-        for (auto sensor : dev.query_sensors())
-        {
-            for (auto profile : sensor.get_stream_profiles())
-            {
-                if (profile.stream_type() == RS2_STREAM_GYRO)
-                    found_gyro = true;
-                if (profile.stream_type() == RS2_STREAM_ACCEL)
-                    found_accel = true;
-            }
-        }
-        if (found_gyro && found_accel)
-            break;
-    }
-    return found_gyro && found_accel;
+	bool found_gyro = false;
+	bool found_accel = false;
+	rs2::context ctx;
+	for (auto dev : ctx.query_devices())
+	{
+		// The same device should support gyro and accel
+		found_gyro = false;
+		found_accel = false;
+		for (auto sensor : dev.query_sensors())
+		{
+			for (auto profile : sensor.get_stream_profiles())
+			{
+				if (profile.stream_type() == RS2_STREAM_GYRO)
+					found_gyro = true;
+				if (profile.stream_type() == RS2_STREAM_ACCEL)
+					found_accel = true;
+			}
+		}
+		if (found_gyro && found_accel)
+			break;
+	}
+	return found_gyro && found_accel;
 }
 
-int main(int argc, char *argv[]) try
+int main(int argc, char* argv[]) try
 
 {
-    using namespace std::chrono_literals;
+	using namespace std::chrono_literals;
 
-    // Before running the example, check that a device supporting IMU is connected
+	// Before running the example, check that a device supporting IMU is connected
 
-    if (!check_imu_is_supported())
+	if (!check_imu_is_supported())
 
-    {
+	{
 
-        std::cerr << "Device supporting IMU (D435i) not found";
+		std::cerr << "Device supporting IMU (D435i) not found";
 
-        return EXIT_FAILURE;
-    }
+		return EXIT_FAILURE;
+	}
 
-    size_t gryro_iter = 0;
-    size_t accel_iter = 0;
-    size_t depth_iter = 0;
-    size_t color_iter = 0;
-    double ts_gyro = 0.0;
-    double ts_accel = 0.0;
-    double ts_depth = 0.0;
-    double ts_color = 0.0;
-    // Create pipeline for gyro and accelerometer. Run in seperate thread.
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
-    rs2::pipeline pipe_motion;
-    // Create a configuration for configuring the pipeline with a non default profile
-    rs2::config cfg_motion;
-    // Add streams of gyro and accelerometer to configuration
-    cfg_motion.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
-    cfg_motion.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
-    // Declare object that handles camera pose calculations
-    rotation_estimator algo;
-    double my_clock = std::chrono::duration<double, std::milli>(std::chrono::system_clock::now().time_since_epoch()).count();
+	size_t gryro_iter = 0;
+	size_t accel_iter = 0;
+	size_t depth_iter = 0;
+	size_t color_iter = 0;
+	double ts_gyro = 0.0;
+	double ts_accel = 0.0;
+	double ts_depth = 0.0;
+	double ts_color = 0.0;
+	double depth_gryo_latency = 0.0;
+	rs2_timestamp_domain gryo_domain;
+	rs2_timestamp_domain accel_domain;
+	rs2_timestamp_domain depth_domain;
+	rs2_timestamp_domain color_domain;
+	// Create pipeline for gyro and accelerometer. Run in seperate thread.
+	// Declare RealSense pipeline, encapsulating the actual device and sensors
+	rs2::pipeline pipe_motion;
+	// Create a configuration for configuring the pipeline with a non default profile
+	rs2::config cfg_motion;
+	// Add streams of gyro and accelerometer to configuration
+	cfg_motion.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
+	cfg_motion.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
+	// Declare object that handles camera pose calculations
+	rotation_estimator algo;
+	double my_clock = std::chrono::duration<double, std::milli>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-    // Start streaming with the given configuration;
-    // Note that since we only allow IMU streams, only single frames are produced
-    auto profile = pipe_motion.start(cfg_motion, [&](rs2::frame frame) {
-        // Cast the frame that arrived to motion frame
-        auto motion = frame.as<rs2::motion_frame>();
-        // If casting succeeded and the arrived frame is from gyro stream
-        if (motion && motion.get_profile().stream_type() == RS2_STREAM_GYRO && motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
-        {
-            // Get the timestamp of the current frame
-            ts_gyro = motion.get_timestamp();
-            auto domain = motion.get_frame_timestamp_domain();
-            // Get gyro measures
-            rs2_vector gyro_data = motion.get_motion_data();
-            // Call function that computes the angle of motion based on the retrieved measures
-            algo.process_gyro(gyro_data, ts_gyro);
-            gryro_iter++;
-        }
-        // If casting succeeded and the arrived frame is from accelerometer stream
-        if (motion && motion.get_profile().stream_type() == RS2_STREAM_ACCEL && motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
-        {
-            // Get accelerometer measures
-            ts_accel = motion.get_timestamp();
-            auto domain = motion.get_frame_timestamp_domain();
-            rs2_vector accel_data = motion.get_motion_data();
-            algo.process_accel(accel_data);
-            accel_iter++;
-        }
-    });
-    // Create pipeline for depth and color. Run in this main thread.
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
-    rs2::pipeline pipe_camera;
-    // Create a configuration for configuring the pipeline with a non default profile
-    rs2::config cfg_camera;
-    // Add streams of gyro and accelerometer to configuration
-    cfg_camera.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
-    cfg_camera.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
-    rs2::pipeline_profile profile_camera;
-    profile_camera = pipe_camera.start(cfg_camera);
-    while (true)
-    {
-        rs2::frameset data = pipe_camera.wait_for_frames();
-        rs2::frame depth = data.get_depth_frame();
-        rs2::frame color = data.get_color_frame();
+	// Start streaming with the given configuration;
+	// Note that since we only allow IMU streams, only single frames are produced
+	auto profile = pipe_motion.start(cfg_motion, [&](rs2::frame frame) {
+		// Cast the frame that arrived to motion frame
+		auto motion = frame.as<rs2::motion_frame>();
+		// If casting succeeded and the arrived frame is from gyro stream
+		if (motion && motion.get_profile().stream_type() == RS2_STREAM_GYRO && motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
+		{
+			// Get the timestamp of the current frame
+			ts_gyro = motion.get_timestamp();
+			gryo_domain = motion.get_frame_timestamp_domain();
+			// Get gyro measures
+			rs2_vector gyro_data = motion.get_motion_data();
+			// Call function that computes the angle of motion based on the retrieved measures
+			algo.process_gyro(gyro_data, ts_gyro);
+			gryro_iter++;
+		}
+		// If casting succeeded and the arrived frame is from accelerometer stream
+		if (motion && motion.get_profile().stream_type() == RS2_STREAM_ACCEL && motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
+		{
+			// Get accelerometer measures
+			ts_accel = motion.get_timestamp();
+			accel_domain = motion.get_frame_timestamp_domain();
+			rs2_vector accel_data = motion.get_motion_data();
+			algo.process_accel(accel_data);
+			accel_iter++;
+		}
+		});
+	// Create pipeline for depth and color. Run in this main thread.
+	// Declare RealSense pipeline, encapsulating the actual device and sensors
+	rs2::pipeline pipe_camera;
+	// Create a configuration for configuring the pipeline with a non default profile
+	rs2::config cfg_camera;
+	// Add streams of gyro and accelerometer to configuration
+	cfg_camera.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
+	cfg_camera.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+	rs2::pipeline_profile profile_camera;
+	profile_camera = pipe_camera.start(cfg_camera);
+	while (true)
+	{
+		rs2::frameset data = pipe_camera.wait_for_frames();
+		rs2::frame depth = data.get_depth_frame();
+		rs2::frame color = data.get_color_frame();
 
-        depth_iter++;
-        color_iter++;
+		depth_iter++;
+		color_iter++;
 
-        ts_depth = depth.get_timestamp();
-        ts_color = color.get_timestamp();
+		ts_depth = depth.get_timestamp();
+		ts_color = color.get_timestamp();
 
-        double new_clock = std::chrono::duration<double, std::milli>(std::chrono::system_clock::now().time_since_epoch()).count();
-        if (new_clock - my_clock >= 1000)
-        {
+		depth_domain = depth.get_frame_timestamp_domain();
+		color_domain = color.get_frame_timestamp_domain();
 
-            my_clock = new_clock;
+		depth_gryo_latency = ts_gyro - ts_depth;
 
-            std::cout << std::setprecision(0) << std::fixed << "FPS --- "
-                      << "Gryo: " << gryro_iter << "; Accel: " << accel_iter
+		double new_clock = std::chrono::duration<double, std::milli>(std::chrono::system_clock::now().time_since_epoch()).count();
+		if (new_clock - my_clock >= 1000)
 
-                      << "; Depth: " << depth_iter << "; RGB: " << color_iter << std::endl;
+		{
 
-            std::cout << std::setprecision(0) << std::fixed << "Timing --- Now: " << my_clock << "; Gryo: " << ts_gyro << "; Accel: " << ts_accel
+			my_clock = new_clock;
 
-                      << "; Depth: " << ts_depth << "; RGB: " << ts_color << std::endl;
+			std::cout << std::setprecision(0) << std::fixed << "FPS --- "
+				<< "Gryo: " << gryro_iter << "; Accel: " << accel_iter
 
-            std::cout << std::endl;
+				<< "; Depth: " << depth_iter << "; RGB: " << color_iter << std::endl;
 
-            gryro_iter = 0;
-            accel_iter = 0;
-            depth_iter = 0;
-            color_iter = 0;
-        }
-        // auto domain = depth.get_frame_timestamp_domain();
-    }
-    // Stop the pipeline
-    pipe_motion.stop();
-    return EXIT_SUCCESS;
+			std::cout << std::setprecision(0) << std::fixed << "Timing --- Now: " << my_clock << "; Gryo: " << ts_gyro << "; Accel: " << ts_accel
+
+				<< "; Depth: " << ts_depth << "; RGB: " << ts_color << std::endl;
+
+			std::cout << std::setprecision(0) << std::fixed << "Time Domain --- Now: " << my_clock << "; Gryo: " << gryo_domain << "; Accel: " << accel_domain
+
+				<< "; Depth: " << depth_domain << "; RGB: " << color_domain << std::endl;
+
+			std::cout << std::setprecision(0) << std::fixed << "Latency --- DepthToGryo: " << depth_gryo_latency << std::endl;
+
+			std::cout << std::endl;
+
+			gryro_iter = 0;
+			accel_iter = 0;
+			depth_iter = 0;
+			color_iter = 0;
+		}
+		// auto domain = depth.get_frame_timestamp_domain();
+	}
+	// Stop the pipeline
+	pipe_motion.stop();
+	return EXIT_SUCCESS;
 }
 
-catch (const rs2::error &e)
+catch (const rs2::error & e)
 
 {
 
-    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+	std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
 
-    return EXIT_FAILURE;
+	return EXIT_FAILURE;
 }
 
-catch (const std::exception &e)
+catch (const std::exception & e)
 
 {
 
-    std::cerr << e.what() << std::endl;
+	std::cerr << e.what() << std::endl;
 
-    return EXIT_FAILURE;
+	return EXIT_FAILURE;
 }
