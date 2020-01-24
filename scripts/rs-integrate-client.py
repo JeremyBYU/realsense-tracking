9559cba976500072cacea3cd69b5e2fad24088cd
Jeremy Castagno <jeremybyu@gmail.com>
2020-01-24 13:41:31 -0500

    C++ Integraton Server Started

--- scripts/rs-integrate-client.py
+++ scripts/rs-integrate-client.py
@@ -5,16 +5,19 @@ import time
 import ecal.core.core as ecal_core
 from ecal.core.service import Client
 
-# THIS_DIR = Path(__file__).parent
-# BUILD_DIR = (THIS_DIR / ".." / "build").resolve()
-# sys.path.insert(1, str(BUILD_DIR))
+THIS_DIR = Path(__file__).parent
+BUILD_DIR = (THIS_DIR / ".." / "build").resolve()
+sys.path.insert(1, str(BUILD_DIR))
 
 
-# from Integrate_pb2 import SceneRequestType, DataRequestType, IntegrateRequest, IntegrateResponse, ADD
+from Integrate_pb2 import SceneRequestType, DataRequestType, IntegrateRequest, IntegrateResponse, ADD, REMOVE
 
 def client_resp_callback(service_info, response):
+    int_resp = IntegrateResponse() 
+    int_resp.ParseFromString(response)
     print(service_info)
     print(response)
+    print(int_resp)
 
 def main():
       # initialize eCAL API
@@ -23,17 +26,20 @@ def main():
     # set process state
     ecal_core.set_process_state(1, 1, "Healthy")
 
-    client = Client("IntegrateService")
+    client = Client("rspub_pb.IntegrateService")
 
       # and add it to the client
     client.add_response_callback(client_resp_callback)
     time.sleep(2)
 
     while True:
-        request_string = bytes("Call Integrate", "ascii")
+        request = IntegrateRequest()
+        request.scene = "Default"
+        request.type = ADD
+        request_string = request.SerializeToString()
+        # request_string = bytes("Call Integrate", "ascii")
         print(request_string)
-        res = client.call_method("IntegrateScene", request_string)
-        print(res)
+        _ = client.call_method("IntegrateScene", request_string)
         time.sleep(1.0)
 
 if __name__ == "__main__":
