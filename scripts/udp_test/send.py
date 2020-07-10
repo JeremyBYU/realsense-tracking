ecff57e47641e01be7866b5cf234693e4cd7e6f8
Jeremy Castagno <jeremybyu@gmail.com>
2020-07-10 13:04:20 -0400

    UDP test scripts

---
+++ scripts/udp_test/send.py
@@ -0,0 +1,13 @@
+import socket
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+# regarding socket.IP_MULTICAST_TTL
+# ---------------------------------
+# for all packets sent, after two hops on the network the packet will not 
+# be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+MULTICAST_TTL = 3
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+sock.sendto(b"robot", (MCAST_GRP, MCAST_PORT))
