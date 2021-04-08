7a2f40460225287198d4216ec43826fd88938824
Jeremy Castagno <jeremybyu@gmail.com>
2021-04-08 16:39:28 -0400

    Add landing service to docker container

---
+++ scripts/launch/launch_landing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# For some reason the rs-integrate service will ONLY work if launched as an entry point!
+# https://github.com/continental/ecal/issues/238
+cd /opt/workspace
+export OMP_NUM_THREADS=1
+echo "Delaying startup for 7 seconds"
+sleep 7
+python -m landing
